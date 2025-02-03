ea156367c353c843fb375a4b387a9f4cfc6cb826
Marat <giliyazovmarat@gmail.com>
2025-02-03 18:33:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,30 +7,38 @@ def log(*message):
         f.write(f'{message}\n')
 
 counter=0
+
+file_text="""apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: public-ip-pool
+  namespace: default
+spec:
+  addresses:
+    - {}/32
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: public-ip-advertisement
+  namespace: default
+spec:
+  ipAddressPools:
+    - public-ip-pool
+
+
+
+"""
+
 while True:
     try:
         ip = subprocess.check_output("kubectl get svc -A | awk '/ingress-nginx-controller/ {print $5}' | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'", shell=True, text=True)
         log(f"line:13 ip: {ip}")
+        print(f"ip:{ip}ip")
         ip = ip.replace('\n',"").strip()
 
         with open("/opt/containerd/lib/manifests/metallb-config.yaml", 'w', encoding='utf-8') as f:
-            f.write(f"""apiVersion: metallb.io/v1beta1
-        kind: IPAddressPool
-        metadata:
-          name: public-ip-pool
-          namespace: default
-        spec:
-          addresses:
-          - {ip}/32
-        ---
-        apiVersion: metallb.io/v1beta1
-        kind: L2Advertisement
-        metadata:
-          name: public-ip-advertisement
-          namespace: default
-        spec:
-          ipAddressPools:
-          - public-ip-pool""")
+            f.write(file_text.format(ip))
 
         log(f"line:34 file done: {ip}")
 
